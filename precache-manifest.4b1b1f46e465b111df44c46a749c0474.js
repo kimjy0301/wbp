e09b8d7316192053d7a6ad3c56dcff15ed892a67
Kim JiYong <znzld505@naver.com>
2020-03-09 17:03:11 +0900

    Updates

---
+++ precache-manifest.4b1b1f46e465b111df44c46a749c0474.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "231c3e2ccab9ba2bd7df2d225073f956",
+    "url": "/wbp/index.html"
+  },
+  {
+    "revision": "7a0bfe8b9212c6b1c7d7",
+    "url": "/wbp/static/css/main.3386b50d.chunk.css"
+  },
+  {
+    "revision": "17513078ece1a93b00fc",
+    "url": "/wbp/static/js/2.b685ed42.chunk.js"
+  },
+  {
+    "revision": "0e3f26916b9527ec0cbade9368e01f25",
+    "url": "/wbp/static/js/2.b685ed42.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a0bfe8b9212c6b1c7d7",
+    "url": "/wbp/static/js/main.b933c103.chunk.js"
+  },
+  {
+    "revision": "daebb0e831e61e1f134a",
+    "url": "/wbp/static/js/runtime-main.adfe019a.js"
+  },
+  {
+    "revision": "f5f6669d23e01b862d0bf07ee442c41d",
+    "url": "/wbp/static/media/bg-about.f5f6669d.jpg"
+  },
+  {
+    "revision": "32af9d3f9707134f6437a25ba4feba60",
+    "url": "/wbp/static/media/bg-business.32af9d3f.jpg"
+  },
+  {
+    "revision": "3d1f052528ce3726a8ed649399400d25",
+    "url": "/wbp/static/media/bg-contact.3d1f0525.jpg"
+  }
+]);
